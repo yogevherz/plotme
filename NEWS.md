9235fff081175962cd054698b82220e8591e655a
Yogev Herz <yogev.herz@riskified.com>
2020-11-03 17:52:29 +0200

    first commit

---
+++ NEWS.md
@@ -0,0 +1,5 @@
+# plotme (development version)
+
+# plotme 0.1.0
+
+* Added a `NEWS.md` file to track changes to the package.
---
+++ R/count_sunburst.R
@@ -0,0 +1,3 @@
+# Takes count_df, barnchvalues (default = "total"), any additional arguments
+# Returns list of args for plot_ly
+# TODO find a way to incorporate colors? Maybe only in path function
