e49b66be2d66cec67915eb5fe59d30714dadd799
Yogev Herz <yogev.herz@riskified.com>
2021-05-27 23:37:29 +0300

    made hover text neater + removed unused root arg

--- R/count_sunburst.R
+++ R/count_sunburst.R
@@ -17,15 +17,15 @@
 #' @export
 #'
 #' @examples
-count_to_sunburst <- function(count_df,
-                              root = NULL){
+count_to_sunburst <- function(count_df){
 
-  params <- create_all_col_params(count_df, root)
+  params <- create_all_col_params(count_df)
 
   purrr::exec(plotly::plot_ly,
               !!!params,
               type = "sunburst",
-              branchvalues = "total")
+              branchvalues = "total",
+              hoverinfo = "text")
 }
 
 
@@ -38,40 +38,33 @@ count_to_sunburst <- function(count_df,
 #' @export
 #'
 #' @examples
-count_to_treemap <- function(count_df,
-                              root = NULL){
+count_to_treemap <- function(count_df){
 
-  params <- create_all_col_params(count_df, root)
+  params <- create_all_col_params(count_df)
 
   purrr::exec(plotly::plot_ly,
               !!!params,
               type = "treemap",
-              branchvalues = "total")
+              branchvalues = "total",
+              hoverinfo = "text")
 }
 
 
-create_all_col_params <- function(count_df,
-                                  root){
+create_all_col_params <- function(count_df){
 
   assert_count_df(count_df)
-  if (!is.null(root)){
-    assertthat::assert_that(is.character(root),
-                            assertthat::is.scalar(root))
-  }
 
   count_df <- all_non_n_cols_to_char(count_df)
 
   category_num <- ncol(count_df) - 1
 
-  if(is.null(root)){
-    root <- ""
-  }
-
   purrr::map(1:category_num,
              create_one_col_params,
              df = count_df,
-             root = root) %>%
-    dplyr::bind_rows()
+             root = "") %>%
+    dplyr::bind_rows() %>%
+    dplyr::mutate(hovertext = stringr::str_glue(
+      "column: {hovertext}\nvalue: {labels}\nn: {values}"))
 }
 
 create_one_col_params <- function(df,
